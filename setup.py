100c89a738b5d3d507674dc17b6794b8004c03b6
Yatao Li <yatli@microsoft.com>
2020-05-18 20:56:16 +0800

    update python module setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extensions = [
         [ "fastBPE/fastBPE." + extension ],
         language='c++',
         extra_compile_args=[
-            "-std=c++11", "-Ofast", "-pthread"
+            "-std=c++17", "-Ofast", "-pthread"
         ],
     ),
 ]
@@ -38,7 +38,7 @@ setup(
     name = 'fastBPE',
     version = '0.1.1',
     description = 'C++ implementation of Neural Machine Translation of Rare Words with Subword Units, with Python API.',
-    url = 'https://github.com/glample/fastBPE',
+    url = 'https://github.com/yatli/fastBPE',
     long_description = readme,
     long_description_content_type = 'text/markdown',
     ext_package = '',
